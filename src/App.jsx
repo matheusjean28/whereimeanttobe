673fc790e64f71584a2e9cf0044b79b0768f3adc
matheusjean28 <matheusjean28@gmail.com>
2025-03-23 16:44:30 -0300

    send rest files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='bg-amber-600'> teste</p>
+      </div>
+    </>
+  )
+}
+
+export default App
